4f6607d90637cda810b7a3e9489359ed1d56b96e
Michael Stranges <strangesm@gmail.com>
2018-03-19 23:03:21 +0000

    able to pull pictures and save in path

--- download_avatars.js
+++ download_avatars.js
@@ -23,9 +23,11 @@ var options = {
 getRepoContributors("jquery", "jquery", function(err, result){
   //used Json to parse the results into an array of objects
   var myJson = JSON.parse(result);
-  //looping through to pick out the avatar_urls and print to screen
+   //looping through to pick out the avatar_urls
+   //function picks out urls and save filepath
+   //sends to function downloadImageByUrl
   for(i = 0; i < myJson.length; i++){
-    console.log(myJson[i].avatar_url)
+      downloadImageByURL(myJson[i].avatar_url, "avatars/" + myJson[i].login + ".jpg")
   }
   //returning my parsed JSON to cb
   return myJson;
@@ -33,8 +35,8 @@ getRepoContributors("jquery", "jquery", function(err, result){
 
 console.log("Welcome to the Github Avatar Downloader!");
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
-
+//requests url
+//gets jpg and saves to filepath
 function downloadImageByURL(url, filepath){
   request.get(url)
     .pipe(fs.createWriteStream(filepath));
