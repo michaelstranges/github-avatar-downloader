d58bfb94ad5dcc1d8ada21ea68a4d3d821b8054b
Michael Stranges <strangesm@gmail.com>
2018-03-19 23:33:45 +0000

    completed project, no add'l modules

--- download_avatars.js
+++ download_avatars.js
@@ -4,53 +4,38 @@ var requestURL = process.argv.slice(2)
 var request = require("request");
 var fs = require("fs");
 
-//throws error if owner empty
-if(requestURL[0] === undefined){
+if(requestURL[0] === undefined){ //throws error if owner empty
   throw new Error("Invalid repository owner");
 }
 
-//throws error if name empty
-if(requestURL[1] === undefined){
+if(requestURL[1] === undefined){ //throws error if name empty
   throw new Error("Invalid repository name");
 }
 
 function getRepoContributors(repoOwner, repoName, cb){
-
-var options = {
-  url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-  headers: {
-    "User-Agent" : "request",
-  //added authorization key using the MyPass module from secrets
-    "Authorization" : myPass.GITHUB_TOKEN
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent" : "request",
+      "Authorization" : myPass.GITHUB_TOKEN //added authorization key using the MyPass module from secrets
   }
 };
-  //request uses options info to achieve connection to server
-  request(options, function(err, res, body){
+  request(options, function(err, res, body){ //request uses options info to achieve connection to server
     cb(err,body);
   });
 }
 
-getRepoContributors(requestURL[0], requestURL[1], function(err, result){
-  //used Json to parse the results into an array of objects
+getRepoContributors(requestURL[0], requestURL[1], function(err, result){ //used Json to parse the results into an array of objects
   var myJson = JSON.parse(result);
-   //looping through to pick out the avatar_urls
-   //function picks out urls and save filepath
-   //sends to function downloadImageByUrl
-  for(i = 0; i < myJson.length; i++){
-      downloadImageByURL(myJson[i].avatar_url, "avatars/" + myJson[i].login + ".jpg")
-  }
-  //returning my parsed JSON to cb
-  return myJson;
+    for(i = 0; i < myJson.length; i++){    //looping through to pick out the avatar_urls
+      downloadImageByURL(myJson[i].avatar_url, "avatars/" + myJson[i].login + ".jpg") //function picks out urls and save filepath
+    }
+  return myJson; //returning my parsed JSON to cb
 });
 
 console.log("Welcome to the Github Avatar Downloader!");
 
-//requests url
-//gets jpg and saves to filepath
-function downloadImageByURL(url, filepath){
+function downloadImageByURL(url, filepath){ //function requests url
   request.get(url)
-    .pipe(fs.createWriteStream(filepath));
+    .pipe(fs.createWriteStream(filepath)); //gets jpg from url and saves to filepath
   }
-
-
-console.log(requestURL[0])
