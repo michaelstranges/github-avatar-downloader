f27e834e3ccea05d96dc7a11c77aee972dff1d7e
Michael Stranges <strangesm@gmail.com>
2018-03-19 22:16:04 +0000

    Parsed my JSON and printed avatar urls.

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var myPass = require("./secrets.js")
+var myJson = {};
 
 var request = require("request");
 
@@ -8,6 +9,7 @@ var options = {
   url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
   headers: {
     "User-Agent" : "request",
+  //added authorization key using the MyPass module from secrets
     "Authorization" : myPass.GITHUB_TOKEN
   }
 };
@@ -18,8 +20,14 @@ var options = {
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
-  console.log("Errors:", err);
-  console.log("Results", result);
+  //used Json to parse the results into an array of objects
+  var myJson = JSON.parse(result);
+  //looping through to pick out the avatar_urls and print to screen
+  for(i = 0; i < myJson.length; i++){
+    console.log(myJson[i].avatar_url)
+  }
+  //returning my parsed JSON to cb
+  return myJson;
 });
 
 console.log("Welcome to the Github Avatar Downloader!");
