d4de30d00b5ac335cc0f35b00b248c0089dafa60
Michael Stranges <strangesm@gmail.com>
2018-03-19 21:19:48 +0000

    updated with gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,25 @@
+var myPass = require("./secrets.js")
+
 var request = require("request");
 
 function getRepoContributors(repoOwner, repoName, cb){
 
+var options = {
+  url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    "User-Agent" : "request",
+    "Authorization" : myPass.GITHUB_TOKEN
+  }
+};
+
+  request(options, function(err, res, body){
+    cb(err,body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors:", err);
-  console.log("Results", results);
+  console.log("Results", result);
 });
 
 console.log("Welcome to the Github Avatar Downloader!");
