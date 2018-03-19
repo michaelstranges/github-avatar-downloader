cfc29ab1dab75d2a5ea3f665cf7dac04cc4690d9
Michael Stranges <strangesm@gmail.com>
2018-03-19 20:50:17 +0000

    just created the function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require("request");
 
+function getRepoContributors(repoOwner, repoName, cb){
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result){
+  console.log("Errors:", err);
+  console.log("Results", results);
+});
+
 console.log("Welcome to the Github Avatar Downloader!");
+
